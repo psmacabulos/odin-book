7a2442e43222c35a689d0160796f863f6e990ccb
psmacabulos <patrick.macabulos@gmail.com>
2023-01-04 11:26:47 +0800

    Added script to create the cards based on myLibrary array

--- index.js
+++ index.js
@@ -17,5 +17,40 @@ let favBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 console.log(favBook.info());
 
 /* Store user input of books into an array */
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: 'The Odin',
+    author: 'Patrick Macabulos',
+    pageNo: 143,
+    readTheBook: true,
+  },
+  {
+    title: 'The Serpent',
+    author: 'Patrick Macabulos',
+    pageNo: 500,
+    readTheBook: false,
+  },
+];
+
 function addBookToLibrary() {}
+
+/* Loop over the myLibrary array and display it */
+const main = document.querySelector('main');
+
+function createBooks(books) {
+  return myLibrary.map(
+    (book) => `
+     <article class="card">
+          <div class="box">
+            <h3 class="title">${book.title}</h3>
+            <p class="author">Patrick Macabulos</p>
+            <p class="pages">153 pages</p>
+            <button class="readingStatus">Finished Reading ?</button>
+            <button class="delete">Delete Book</button>
+          </div>
+        </article>
+  `
+  );
+}
+
+main.innerHTML = createBooks(myLibrary);
